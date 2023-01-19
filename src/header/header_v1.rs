588ea7e0e38f83a0d3e216dbb3309f2372f44fc7
gongchengzhi <gongcz@koal.com>
2023-01-19 17:52:00 +0800

    init

--- src/header/header_v1.rs
+++ src/header/header_v1.rs
@@ -1,7 +1,6 @@
-use libipld::Cid;
+use cid::Cid;
 
-
-#[derive(Debug, Clone, PartialEq, Eq, Default, libipld::DagCbor)]
+#[derive(Debug, Clone, PartialEq, Eq, Default, ipld::DagCbor)]
 pub struct CarHeaderV1 {
     #[ipld]
     pub roots: Vec<Cid>,
@@ -28,9 +27,15 @@ impl From<Vec<Cid>> for CarHeaderV1 {
 #[cfg(test)]
 mod test {
     use super::*;
+    use ipld::codec::{Decode, Encode};
+    use ipld_cbor::DagCborCodec;
+    use cid::Cid;
+    use cid::multihash::{MultihashDigest, Code::Blake2b256};
 
     #[test]
     fn test_head_v1() {
+        let digest = Blake2b256.digest(b"test");
+        let cid = Cid::new_v1(DagCborCodec.into(), digest);
         
     }
 
