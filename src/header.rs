f6c722178fb5c3995047fe090d9a385939244d5f
gongchengzhi <gongcz@koal.com>
2023-01-19 18:04:47 +0800

    init

--- src/header.rs
+++ src/header.rs
@@ -1,11 +1,16 @@
 
 mod header_v1;
 use header_v1::CarHeaderV1;
+
+use cid::Cid;
+
 pub enum CarHeader {
     V1(CarHeaderV1),
     V2(),
 }
 
 impl CarHeader {
-    
+    pub fn new_v1(roots: Vec<Cid>) -> Self {
+        CarHeader::V1(CarHeaderV1::new(roots))
+    }
 }
