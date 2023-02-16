1352b4217f8cbf8d2a238c27df5001e1b8ce84b9
gongchengzhi <gongcz@koal.com>
2023-02-16 12:22:04 +0800

    support deeep search file

--- examples/cat_file.rs
+++ examples/cat_file.rs
@@ -8,7 +8,8 @@ use rust_car::{
 /// the example cat used file is carv1-basic.car
 fn main() {
     let file_name = std::env::args().nth(1).expect("use filename as argument");
-    let file = std::path::Path::new("/Users/join/Downloads/my-files.car");
+    let file = std::path::Path::new("test");
+    let file = file.join("carv1-basic.car");
     let file = std::fs::File::open(file).unwrap();
     let mut reader = reader::new_v1(file).unwrap();
     let cid = reader.search_file_cid(&file_name).expect("search file error.");
