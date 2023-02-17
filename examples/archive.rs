ef7d6c6946d1cf31713db0a75fa9d668a5030f4a
gongchengzhi <gongcz@koal.com>
2023-02-17 22:11:26 +0800

    support archive file

--- examples/archive.rs
+++ examples/archive.rs
@@ -3,7 +3,7 @@ use rust_car::{
 };
 
 /// Cat the file in car file by file id
-/// e.g. ```cargo run --example archive directory `target file`.```
+/// e.g. ```cargo run --example `archive directory` `target car file`.```
 fn main() {
     let file_name = std::env::args().nth(1).expect("use directory as argument");
     let target = std::env::args().nth(2).expect("need the target file as argument");
