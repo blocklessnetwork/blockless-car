4ee889b942e28159412521d931b7c2043115a319
joinhack <joinhack@gmail.com>
2023-03-11 07:04:44 +0800

    add comment and the  handling incase

--- src/utils/ls.rs
+++ src/utils/ls.rs
@@ -3,22 +3,24 @@ use std::collections::{VecDeque, HashMap};
 use cid::Cid;
 use ipld::raw::RawCodec;
 
-use crate::{reader::CarReader, Ipld, unixfs::{UnixFs, FileType}};
+use crate::{reader::CarReader, Ipld, unixfs::{UnixFs, FileType}, error::CarError};
 
 /// walk the node and print the files in the directory.
-fn walk(vecq: &mut VecDeque<Cid>, reader: &mut impl CarReader) {
+fn walk(vecq: &mut VecDeque<Cid>, reader: &mut impl CarReader) -> Result<(), CarError> {
     let mut cache: HashMap<Cid, String> = HashMap::new();
     let raw_code: u64 = RawCodec.into();
     while let Some(file_cid) = vecq.pop_front() {
         let codec = file_cid.codec();
         let file_n = cache.get(&file_cid).map_or(file_cid.to_string(), |c| c.to_string());
         println!("{file_n}");
+        // if the codec is RawCodec, the block is the file content block, 
+        // it don't conatian the file info. we don't need walk continue.
         if codec == raw_code {
             continue;
         }
-        let file_ipld: Ipld = reader.ipld(&file_cid).unwrap();
+        let file_ipld: Ipld = reader.ipld(&file_cid)?;
         if let m @  Ipld::Map(_) = file_ipld {
-            let unixfs: UnixFs = m.try_into().unwrap();
+            let unixfs: UnixFs = m.try_into()?;
             match unixfs.file_type() {
                 FileType::Directory => {},
                 _=> continue,
@@ -30,14 +32,16 @@ fn walk(vecq: &mut VecDeque<Cid>, reader: &mut impl CarReader) {
             }
         }
     }
+    Ok(())
 }
 
 /// the list car file  by reader.
-pub fn list(reader: &mut impl CarReader) {
+pub fn list(reader: &mut impl CarReader)  -> Result<(), CarError> {
     let roots = reader.header().roots();
     let mut queue: VecDeque<Cid> = VecDeque::new();
     for r in roots.iter() {
         queue.push_front(*r);
-        walk(&mut queue, reader);
+        walk(&mut queue, reader)?;
     }
+    Ok(())
 }
